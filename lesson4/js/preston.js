be2d428429618facf3d25b33b1442a89d726d339
jessicaingersoll <jessica.pasternak12@gmail.com>
2021-05-14 14:46:07 -0400

    Enter

--- lesson4/js/preston.js
+++ lesson4/js/preston.js
@@ -7,5 +7,10 @@ hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive'
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
 
 const d = new Date();
-const currentdate = d.
+const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+const currentdate = weekday[d.getDay()]+","+d.getDate()+month[d.getMonth()]+ d.getFullYear()
+
 document.querySelector('#currentdate').textContent = currentdate;
+
+Wednesday, 24 July 2020
