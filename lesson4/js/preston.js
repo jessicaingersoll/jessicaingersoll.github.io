8eae60b910d5223c185cec3912a44744402c119b
jessicaingersoll <jessica.pasternak12@gmail.com>
2021-05-13 21:27:25 -0400

    Enter

--- lesson4/js/preston.js
+++ lesson4/js/preston.js
@@ -6,4 +6,6 @@ hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive'
 // To solve the mid resizing issue with responsive class on
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
 
-var d = new Date();
+const d = new Date();
+const currentdate = d.
+document.querySelector('#currentdate').textContent = currentdate;
