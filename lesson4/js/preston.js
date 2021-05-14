bb0d5a3338267bb12eb47d3e473b7fd0369d912a
jessicaingersoll <jessica.pasternak12@gmail.com>
2021-05-14 14:52:01 -0400

    Enter

--- lesson4/js/preston.js
+++ lesson4/js/preston.js
@@ -9,7 +9,7 @@ window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('
 const d = new Date();
 const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-const currentdate = weekday[d.getDay()]+","+d.getDate()+month[d.getMonth()]+ d.getFullYear()
+const currentdate = weekday[d.getDay()]+", "+d.getDate()+month[d.getMonth()]+ d.getFullYear()
 
 document.querySelector('#currentdate').textContent = currentdate;
 
