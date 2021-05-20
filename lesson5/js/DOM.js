301fada36e287062b215439f4e3eea5682f85442
jessicaingersoll <jessica.pasternak12@gmail.com>
2021-05-19 20:13:35 -0400

    Enter

--- lesson5/js/DOM.js
+++ lesson5/js/DOM.js
@@ -2,18 +2,21 @@ const input = document.querySelector('input');
 const button = document.querySelector('button');
 const list = document.querySelector('ul');
 button.addEventListener('click',function() {
-    const listItem = document.createElement('li');
-    const listText = document.createElement('span');
-    const listBnt = document.createElement('button');
+    if (input.value != "") {
+        let myItem = input.value;
+        input.value = "";
+        const listItem = document.createElement('li');
+        const listText = document.createElement('span');
+        const listBtn = document.createElement('button');
 
-    listItem.appendChild(listText);
-    listText.textContent = myItem;
-    listItem.appendChild(listBtn);
-    listBtn.textContent = 'Delete ❌';
-    list.appendChild(listItem);
+        listItem.appendChild(listText);
+        listText.textContent = myItem;
+        listItem.appendChild(listBtn);
+        listBtn.textContent = 'Delete ❌';
+        list.appendChild(listItem);
 
-    listBtn.onclick = function(e) {
-        list.removeChild(listItem);
-    }
-    input.focus();
-})
+        listBtn.onclick = function(e) {
+            list.removeChild(listItem);
+        }
+        input.focus();
+}})
