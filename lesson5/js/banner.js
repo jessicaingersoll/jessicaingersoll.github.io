443d3d73206b7eb437754c7e8193426f326f8a94
jessicaingersoll <jessica.pasternak12@gmail.com>
2021-05-22 18:03:38 -0400

    Enter

--- lesson5/js/banner.js
+++ lesson5/js/banner.js
@@ -1,8 +1,8 @@
 //determine day of week
 let thedate = new Date();
-//console.log(thedate.getDay());
+console.log(thedate.getDay());
 
 //if it is friday then turn the aside display property to block
-if (thedate.getDay() == 7) [
+if (thedate.getDay() === 6) {
     document.querySelector("#banner").style.display = "block"
-]
+}
