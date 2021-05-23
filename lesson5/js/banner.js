532dfe721005f19cc9d61b4e1ec771c35a6eb1d8
jessicaingersoll <jessica.pasternak12@gmail.com>
2021-05-22 20:05:20 -0400

    Enter

--- lesson5/js/banner.js
+++ lesson5/js/banner.js
@@ -3,6 +3,6 @@ let thedate = new Date();
 console.log(thedate.getDay());
 
 //if it is friday then turn the aside display property to block
-if (thedate.getDay() === 6) {
+if (thedate.getDay() == 5) {
     document.querySelector("#banner").style.display = "block"
 }
