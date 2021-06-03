8d27dc8d80deab49d2b5d795bd7775c4029e5331
jessicaingersoll <jessica.pasternak12@gmail.com>
2021-06-03 17:20:46 -0400

    Enter

--- lesson7/js/lazy.js
+++ lesson7/js/lazy.js
@@ -9,11 +9,23 @@ imagesToLoad.forEach((img) => {
     loadImages(img);
 });
 
-
-
-
-
-
+if('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if(item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
+        });
+    });
+    imagesToLoad.forEach((img) => {
+        observer.observe(img);
+    });
+} else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}
 
 
 
