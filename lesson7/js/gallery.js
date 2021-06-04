f3c0cca235a466f4e9c8821f7cfe3c888b808b1e
jessicaingersoll <jessica.pasternak12@gmail.com>
2021-06-04 19:19:04 -0400

    Enter

--- lesson7/js/gallery.js
+++ lesson7/js/gallery.js
@@ -32,3 +32,34 @@ imagesToLoad.forEach((img) => {
         loadImages(img);
     });
 }
+
+if(localStorage) {
+    var lastVisitMessage = "";
+    var lastVisit = localStorage.getItem('lastVisit');
+    if (lastVisit == null) {
+        lastVisitMessage = "Welcome to our site! Enjoy your first visit!";
+    }
+    else {
+        var todaysDate = new Date();
+        var numberOfDays = getNumberOfDays(lastVisit, todaysDate);
+        lastVisitMesssage = "The last time you visited this site was" + numberOfDays + "days ago!";
+    }
+
+    localStorage.setItem('lastVisit',(new Date()).getTime());
+    document.getElementById("daysbetween").innerHTML = `${lastVisitMessage}`;
+}
+
+
+
+function getNumberOfDays(lastVisitTime, todaysDate) {
+    const lastVisit = ('lastVisitTime');
+    const now = Date.now();
+
+    const oneDay = 1000 * 60 * 60 * 24;
+
+    const diffInTime = date2.getTime() - date1.getTime();
+
+    const diffInDays = Math.round(diffInTime/oneDay);
+
+    return diffInDays;
+}
