a8266d1dd6579da6ecb8e07d7ce0f1d457ca994b
jessicaingersoll <jessica.pasternak12@gmail.com>
2021-06-15 19:48:05 -0400

    Enter

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -19,8 +19,8 @@ fetch(requestURL)
       h2.textContent = prophets[i].name + " " + prophets[i].lastname;
       p.textContent = "Date of Birth: " + prophets[i].birthdate;
       p2.textContent = "Place of Birth: " + prophets[i].birthplace;
-      img.setAttribute('src', prophets[i].imageurl);
-      alt.setAttribute('alt', prophets[i].name + " " + prophets[i].lastname + "-" + prophets[i].order);
+      /*img.setAttribute('src', prophets[i].imageurl);
+      alt.setAttribute('alt', prophets[i].name + " " + prophets[i].lastname + "-" + prophets[i].order); */
 
       card.appendChild(h2);
       card.appendChild(p);
