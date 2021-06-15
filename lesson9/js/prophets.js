ce3ff2605fff3bed9db2f4d1886d670fa4a602f4
jessicaingersoll <jessica.pasternak12@gmail.com>
2021-06-15 19:44:20 -0400

    Enter

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -13,11 +13,12 @@ fetch(requestURL)
       let h2 = document.createElement('h2');
       let p = document.createElement('p');
        let p2 = document.createElement('p');
+     let image = document.createElement('img');
 
       h2.textContent = prophets[i].name + " " + prophets[i].lastname;
-      p.textContent = "Date of Birth:" + prophets[i].birthdate;
-      p2.textContent = "Place of Birth:" + prophets[i].birthplace;
-
+      p.textContent = "Date of Birth: " + prophets[i].birthdate;
+      p2.textContent = "Place of Birth: " + prophets[i].birthplace;
+      image.setAttribute('src', prophets[i].imageurl);
 
       card.appendChild(h2);
       card.appendChild(p);
