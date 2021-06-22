e25d73f8246c97349fa6b30901dc9b5fd75f0f69
jessicaingersoll <jessica.pasternak12@gmail.com>
2021-06-22 16:08:07 -0400

    Enter

---
+++ lesson10/js/weatherapi.js
@@ -0,0 +1,8 @@
+const apiURL =
+ 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=f76def132e0211ea6635d4837cd91bdb';
+
+ fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+  });
