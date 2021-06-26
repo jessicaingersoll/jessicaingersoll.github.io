99a0efdb63fc0babe0dd954d2bb74ac794eb9e9b
jessicaingersoll <jessica.pasternak12@gmail.com>
2021-06-25 21:09:56 -0400

    Enter

--- lesson10/js/preston.js
+++ lesson10/js/preston.js
@@ -3,6 +3,7 @@ const appid = "f76def132e0211ea6635d4837cd91bdb";
 
 const apiURL =
     `http://api.openweathermap.org/data/2.5/weather?id=${cityid}&appid=${appid}&units=imperial`;
+
  fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
@@ -10,5 +11,5 @@ const apiURL =
     document.getElementById('temperature').textContent = jsObject.main.temp_max; 
     document.getElementById('humidity').textContent = jsObject.main.humidity;
     document.getElementById('windspeed').textContent = jsObject.wind.speed;
-    document.getElementById('description').textContent = jsObject.weather[0].main;
+    document.getElementById('description').textContent = jsObject.weather[0].description;
   });
