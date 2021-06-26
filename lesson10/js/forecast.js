9ad49d753789f879804cc022fb33ce9d6612ffdf
jessicaingersoll <jessica.pasternak12@gmail.com>
2021-06-25 21:29:49 -0400

    Enter

---
+++ lesson10/js/forecast.js
@@ -0,0 +1,17 @@
+const cityid = "5604473";
+const appid = "f76def132e0211ea6635d4837cd91bdb";
+
+const apiURL =
+    `http://api.openweathermap.org/data/2.5/forecast?id=${cityid}&appid=${appid}&units=imperial`;
+
+ fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    let day = 0;
+    const dayofWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'];
+    thefive.forEach( x => {
+        let d = new Date(x.dt_txt);
+        console.log(d);
+    });
+});
