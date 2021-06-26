5362127234c230d452bae1b5617de2287f8af568
jessicaingersoll <jessica.pasternak12@gmail.com>
2021-06-25 21:41:35 -0400

    Enter

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -4,8 +4,6 @@ const apiURL =
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
-  
-
     document.getElementById('current-temp').textContent = jsObject.main.temp;
     const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
 const desc = jsObject.weather[0].description;  // note how we reference the weather array
@@ -13,4 +11,12 @@ document.getElementById('imagesrc').textContent = imagesrc;  // informational sp
 document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
 document.getElementById('icon').setAttribute('alt', desc);
 
+let day = 0;
+    const dayofWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'];
+    thefive.forEach( x => {
+        let d = new Date(x.dt_txt);
+        //console.log(d);
+        document.getElementById(`dayofweek${day+1}`).textContent = dayofWeek[d.getDay()];
+        document.getElementById(`forecast${day+1}`).textContent = x.main.temp;
+        day++
   });
