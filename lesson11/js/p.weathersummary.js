93b3efaae9d646223e5cbf748a39dca0a24f23e0
jessicaingersoll <jessica.pasternak12@gmail.com>
2021-06-29 21:29:23 -0400

    Enter

--- lesson11/js/p.weathersummary.js
+++ lesson11/js/p.weathersummary.js
@@ -1,5 +1,15 @@
-const cityid = "5604473";
+
 const appid = "f76def132e0211ea6635d4837cd91bdb";
+
+if (document.getElementById('city').textContent == "Preston") {
+  let cityid = "5604473";
+}
+else if (document.getElementById('city').textContent == "Soda Springs") {
+  let cityid = "5607916";
+}
+else {
+  let cityid = "5585010";
+}
 
 const apiURL =
     `https://api.openweathermap.org/data/2.5/weather?id=${cityid}&appid=${appid}&units=imperial`;
@@ -13,3 +23,21 @@ const apiURL =
     document.getElementById('windspeed').textContent = Math.round(jsObject.wind.speed);
     document.getElementById('description').textContent = jsObject.weather[0].description.toUpperCase();
   });
+
+
+
+  /*const cityid = "5604473";
+const appid = "f76def132e0211ea6635d4837cd91bdb";
+
+const apiURL =
+    `https://api.openweathermap.org/data/2.5/weather?id=${cityid}&appid=${appid}&units=imperial`;
+
+ fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    document.getElementById('temperature').textContent = Math.round(jsObject.main.temp_max); 
+    document.getElementById('humidity').textContent = jsObject.main.humidity;
+    document.getElementById('windspeed').textContent = Math.round(jsObject.wind.speed);
+    document.getElementById('description').textContent = jsObject.weather[0].description.toUpperCase();
+  }); */
