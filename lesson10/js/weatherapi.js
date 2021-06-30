9fff634c3093031a5be40d37dc0feacb62a8f11e
jessicaingersoll <jessica.pasternak12@gmail.com>
2021-06-29 20:09:17 -0400

    Enter

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -20,3 +20,19 @@ let day = 0;
         document.getElementById(`forecast${day+1}`).textContent = x.main.temp;
         day++
   });
+
+let temp = parseFloat(document.querySelector('#temperature').textContent);
+let speed = parseFloat(document.querySelector('#windspeed').textContent);
+
+if (temp <= 50 && speed >= 3) {
+    let wc = windChill(temp, speed);
+    document.querySelector('.windchill').textContent = wc.toFixed(0);
+}
+else {
+    document.querySelector('.windchill').textContent = "N/A";
+}
+
+function windChill(temp, speed) {
+    let wchill = 35.74 + .6215 * temp - 35.75 * (Math.pow(speed, .16)) + .4275 * temp * (Math.pow(speed, .16));
+    return wchill;
+}
