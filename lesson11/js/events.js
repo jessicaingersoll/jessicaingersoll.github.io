7ea9a9db05b4abf062af8b4418f54d77e17394fb
jessicaingersoll <jessica.pasternak12@gmail.com>
2021-07-02 19:15:47 -0400

    Enter

--- lesson11/js/events.js
+++ lesson11/js/events.js
@@ -9,42 +9,10 @@ var requestURL =
   })
   .then(function (jsonObject) {
     var towns = jsonObject['towns'];
-    var pagename = document.getElementById('.city').innerHTML;
-    console.log(pagename);
+    var pagename = document.getElementById('city').innerHTML;
+    
     for (let i = 0; i < towns.length; i++) {
-      if (towns[i].name == "Soda Springs") {
-        let events = document.createElement('section');
-        let p = document.createElement('p');
-
-        p.textContent = towns[i].events;
-
-        events.appendChild(p);
-       console.log(events);
-
-        document.querySelector('div.sevents').appendChild(events);
+      if (towns[i].name == pagename) {
+        console.log(towns[i].events)
         
-      }
-      else if ( towns[i].name == "Fish Haven"){
-        let events = document.createElement('section');
-        let p = document.createElement('p');
-
-        p.textContent = towns[i].events;
-
-        events.appendChild(p);
-        console.log(events);
-        document.querySelector('div.fevents').appendChild(events);
-      }
-      else if (towns[i].name == "Preston") {
-      let events = document.createElement('section');
-        let p = document.createElement('p');
-
-        p.textContent = towns[i].events;
-
-        events.appendChild(p);
-        console.log(events);
-        document.querySelector('div.pevents').appendChild(events);
-     }
-    };
-
-   
-})
+      }}});
