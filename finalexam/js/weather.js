d2cdf7165bf688d25be83b0958cdaade8f7998ba
jessicaingersoll <jessica.pasternak12@gmail.com>
2021-07-12 16:31:05 -0400

    Enter

--- finalexam/js/weather.js
+++ finalexam/js/weather.js
@@ -1,3 +1,4 @@
+
 var apikey = "f76def132e0211ea6635d4837cd91bdb";
 
 const apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=25.7906&lon=-80.1301&&appid=${apikey}&units=imperial`;
@@ -10,7 +11,7 @@ fetch(apiURL)
     document.getElementById('low').textContent = Math.round(jsObject.daily[0].temp.min);
     document.getElementById('humidity').textContent = jsObject.current.humidity;
     document.getElementById('currenttemp').textContent = Math.round(jsObject.current.temp);
-    document.getElementById('currenticon').setAttribute('src', `https://openweathermap.org/img/wn/weather[0].icon}.png`);
+    document.getElementById('currenticon').setAttribute('src', `https://openweathermap.org/img/w/weather[0].icon}.png`);
     document.getElementById('currenticon').setAttribute('alt', 'N/A');
     
   });
