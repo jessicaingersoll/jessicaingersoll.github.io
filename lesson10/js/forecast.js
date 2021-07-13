29a19c425968053e68cd4afc07aacbe414cdc821
jessicaingersoll <jessica.pasternak12@gmail.com>
2021-07-13 13:53:51 -0400

    Enter

--- lesson10/js/forecast.js
+++ lesson10/js/forecast.js
@@ -15,7 +15,7 @@ const apiURL2 =
     console.log(fiveDayForecast);
     fiveDayForecast.forEach( x => {
         let d = new Date(x.dt_txt);
-        //console.log(d);
+        console.log(d);
         document.getElementById(`dayofweek${day+1}`).textContent = dayofWeek[d.getDay()];
         document.getElementById(`forecast${day+1}`).textContent = Math.round(x.main.temp);
         document.getElementById(`icon${day+1}`).setAttribute('src', `https://openweathermap.org/img/w/${x.weather[0].icon}.png`);
