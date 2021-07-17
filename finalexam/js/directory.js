8eb141b6909ce436292be8f0ed75f9500356720c
jessicaingersoll <jessica.pasternak12@gmail.com>
2021-07-17 18:00:51 -0400

    Enter

--- finalexam/js/directory.js
+++ finalexam/js/directory.js
@@ -1,2 +1,11 @@
-const file = 'json/business.json';
+const file = require(''./json/business.json');
 console.log(file);
+
+ fetch(file)
+ .then(function (resonse) {
+     return response.json();
+ })
+ .then(function(jsonObject) {
+    const business = jsonObject['business'];
+    console.log(business);
+ })
