437898db1f026d6bf8a90e81d901e230df1bda2b
jessicaingersoll <jessica.pasternak12@gmail.com>
2021-07-17 22:39:55 -0400

    Enter

--- finalexam/js/directorygrid.js
+++ finalexam/js/directorygrid.js
@@ -2,3 +2,7 @@ function gridView() {
     var element = document.getElementById("bd");
    element.classList.toggle("gridstyle");
 }
+function listView() {
+    var element = document.getElementById("bd");
+    element.classList.toggle("liststyle");
+}
