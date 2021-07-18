79c36998f654cbf7f52a63d080589000d9808961
jessicaingersoll <jessica.pasternak12@gmail.com>
2021-07-17 22:38:23 -0400

    Enter

--- finalexam/js/directorygrid.js
+++ finalexam/js/directorygrid.js
@@ -1,7 +1,4 @@
-const gridbutton = document.querySelector('.gridview');
-const listview = document.querySelector('.listview')
-
-hambutton.addEventListener('click', () => {listview.classList.toggle('responsive')}, false);
-
-// To solve the mid resizing issue with responsive class on
-window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+function gridView() {
+    var element = document.getElementById("bd");
+   element.classList.toggle("gridstyle");
+}
