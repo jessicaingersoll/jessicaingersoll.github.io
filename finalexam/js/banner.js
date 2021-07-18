25f6dd43e1bec0294b9857bd276a1d7ca0f196c4
jessicaingersoll <jessica.pasternak12@gmail.com>
2021-07-18 10:53:13 -0400

    Enter

--- finalexam/js/banner.js
+++ finalexam/js/banner.js
@@ -1,6 +1,6 @@
 let thedate = new Date();
 console.log(thedate.getDay());
 
-if (thedate.getDay() == 1) {
+if (thedate.getDay() == 0) {
     document.querySelector("#banner").style.display = "block"
 }
