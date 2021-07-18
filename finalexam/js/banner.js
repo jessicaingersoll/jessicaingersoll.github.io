df2672d4a3267cc87e477e69db178bdb36ca1c37
jessicaingersoll <jessica.pasternak12@gmail.com>
2021-07-18 12:46:13 -0400

    ENter

--- finalexam/js/banner.js
+++ finalexam/js/banner.js
@@ -5,3 +5,10 @@ if (thedate.getDay() == 0) {
     document.querySelector("#banner").style.display = "block"
 };
 const div = document.querySelector("#banner");
+const btn = document.querySelector("bannerbtn");
+
+btn.addEventListener('click', () =>{
+    while (div.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+})
